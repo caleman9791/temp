623174a6b9aedebcfc4280c86dfbea6e3b4e0893
Carlos Aleman <caleman9791@protonmail.com>
2023-05-20 22:54:28 -0400

    ok

--- docs/js/lib/js.js
+++ docs/js/lib/js.js
@@ -41,11 +41,11 @@ function deleteCookie(name) {
 function generateP() {
 	var pass = '';
 	var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$';
-	// var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	// var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$';
 
 	for (let i = 1; i <= 30; i++) {
-		var char = Math.floor(Math.random() *
-			str.length + 1);
+		var char = Math.floor(Math.random()
+			* str.length + 1);
 
 		pass += str.charAt(char)
 	}
